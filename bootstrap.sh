029059e41cd4eddd24675ed65ae696344e788e6b
amberg12 <>
2024-05-07 17:46:09 +0100

    began nvim config

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# A bootstrap script to setup the .dotfiles folder.
+# This file is licensed under the Do What The Fuck You Want To Public License.
+
+ln -s ./nvim ~/.config/nvim
