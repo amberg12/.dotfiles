419f1f4e30710eeaab05aaebf1cf0304849cd852
amberg12 <>
2024-05-09 13:43:38 +0100

    added treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,22 +6,25 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+vim.wo.relativenumber = true
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     { "nvim-telescope/telescope.nvim", tag = "0.1.6", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 }
 local opts = {}
 
@@ -31,3 +34,10 @@ vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+    ensure_installed = { "lua", "c", "rust" },
+    highlight = { enable = true },
+    indent = { enable = true },
+})
