035a540a212d9b8ee8935d3295df47ad82eec425
amberg12 <>
2024-05-10 10:22:48 +0100

    added python to languages

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require('nvim-treesitter.configs')
     config.setup({
-      ensure_installed = { 'lua', 'c', 'rust' },
+      ensure_installed = { 'lua', 'c', 'rust', 'python' },
       highlight = { enable = true },
       indent = { enable = true },
     })
