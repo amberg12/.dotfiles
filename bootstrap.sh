858ab3adc1c379b817390006700656110a2c3a2c
amber <>
2024-05-11 17:16:41 +0100

    fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,13 +4,14 @@
 # This file is licensed under the BSD Zero Clause License. 
 
 read -n1 -p "Would you like to install the nvim config (will overwrite)? [Y/n]" instnvim
+echo "\n"
 case $instnvim in
     y|Y)
-        echo "Deleting config..."
-        rm -r ~/.config/nvim 
-        ln -s ./nvim ~/.config/nvim
+        echo "Deleting config...\n"
+        rm -r -f ~/.config/nvim 
+	ln -s $(pwd)/nvim ~/.config/nvim
         ;;
     *) # Take everything else to be a no
-        echo "Skipping nvim config..."
+        echo "Skipping nvim config...\n"
         ;;
 esac
