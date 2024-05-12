5c73ba17fd3c760df523daf4118d55d6fe9a1fc7
amber <>
2024-05-12 13:04:15 +0100

    tabs -> spaces

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,33 +1,33 @@
 return {
   {
-  	"nvim-telescope/telescope.nvim",
-  	tag = "0.1.6",
-  	dependencies = { "nvim-lua/plenary.nvim" },
-  	config = function()
-  		local builtin = require("telescope.builtin")
-  		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-  		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-  	end,
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    end,
   },
   {
-  	"nvim-telescope/telescope-ui-select.nvim",
-  	config = function()
-  		require("telescope").setup({
-  			extensions = {
-  				["ui-select"] = {
-  					require("telescope.themes").get_dropdown({}),
-  				},
-  			},
-  		})
-  	end,
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+    end,
   },
   {
-  	"nvim-telescope/telescope-file-browser.nvim",
-  	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-  	config = function()
-  		require("telescope").setup({
-  			vim.keymap.set("n", "<C-l>", ":Telescope file_browser<CR>"),
-  		})
-  	end,
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        vim.keymap.set("n", "<C-l>", ":Telescope file_browser<CR>"),
+      })
+    end,
   },
 }
