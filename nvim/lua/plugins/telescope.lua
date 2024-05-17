d1721f2b803c83385b4610a9c52f8d175a5b5ed1
amberg12 <>
2024-05-17 17:52:59 +0100

    fixed the last change

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,12 +5,7 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set(
-				"n",
-				"<C-p>",
-				builtin.find_files({ find_command = { "rg", "--files", "--hidden", "-g", "!.git" } }),
-				{}
-			)
+			vim.keymap.set("n", "<C-p>", "<cmd> lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 		end,
 	},
