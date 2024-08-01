7a7afe18fb9184b0a602fcbe9eb74e0a63fad9e3
Amber <amber.g.dev@proton.me>
2024-08-01 21:59:29 +0100

    indent

--- nvim/lua/x/lazy/treesitter.lua
+++ nvim/lua/x/lazy/treesitter.lua
@@ -1,11 +1,11 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
-			highlight = { enable = true },
-			indent = { enable =  true },
-		})
-	end
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
+      highlight = { enable = true },
+      indent = { enable =  true },
+    })
+  end
 }
--- nvim/lua/x/lsp_config.lua
+++ nvim/lua/x/lsp_config.lua
@@ -5,32 +5,32 @@ local cmp_select = { behaviour = cmp.SelectBehavior.Select }
 
 -- :help lsp-zero-keybindings
 lsp_zero.on_attach(function(_, bufnr)
-	lsp_zero.default_keymaps({ buffer = bufnr })
+  lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 cmp.setup({
-	mapping = cmp.mapping.preset.insert({
-		['<C-p>'] = cmp.mapping.select_next_item(cmp_select),
-		['<C-S-p>'] = cmp.mapping.select_prev_item(cmp_select),
-		['<C-y>'] = cmp.mapping.confirm({ select = true }),
-		['<C-Space>'] = cmp.mapping.complete(),
-	}),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-S-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
 })
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = { "lua_ls", "clangd" },
-	handlers = {
-		function(server_name)
-			if server_name == 'lua_ls' then
-				require('lspconfig').lua_ls.setup({
-					-- Lisp looking line
-					settings = { Lua = { diagnostics = { globals = { "vim" } } } }
-				})
-			else
-				-- Most plugins can be automatically configured like this.
-				require('lspconfig')[server_name].setup({})
-			end
-		end
-	}
+  ensure_installed = { "lua_ls", "clangd" },
+  handlers = {
+    function(server_name)
+      if server_name == 'lua_ls' then
+        require('lspconfig').lua_ls.setup({
+          -- Lisp looking line
+          settings = { Lua = { diagnostics = { globals = { "vim" } } } }
+        })
+      else
+        -- Most plugins can be automatically configured like this.
+        require('lspconfig')[server_name].setup({})
+      end
+    end
+  }
 })
