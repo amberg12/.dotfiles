418a2936190b9d91dee0575c13714542656cb8b7
Amber <amber.g.dev@proton.me>
2024-08-02 01:44:37 +0100

    fix telescope

--- nvim/lua/x/lazy/telescope.lua
+++ nvim/lua/x/lazy/telescope.lua
@@ -2,8 +2,18 @@ return {{
   "nvim-telescope/telescope.nvim",
   dependencies = { { "nvim-lua/plenary.nvim" } },
   config = function()
+    require("telescope").setup({
+      -- There should be no reason to need .git
+      defaults = { file_ignore_patterns = { ".git" } },
+    })
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+    vim.keymap.set("n", "<C-p>",
+      function()
+        builtin.find_files({
+          hidden = true,
+          no_ignore = false,
+})
+      end, {})
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
   end
 }}
