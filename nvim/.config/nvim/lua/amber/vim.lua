c0362eba3d9290e67fbd9eb46363b539c96e72c3
Amber Goulding <gouldinga1@cardiff.ac.uk>
2024-11-18 18:20:39 +0000

    add nicer tag bindings

--- nvim/.config/nvim/lua/amber/vim.lua
+++ nvim/.config/nvim/lua/amber/vim.lua
@@ -19,3 +19,16 @@ vim.keymap.set({'n', 'v'}, '<leader>p', [["+p]])
 vim.keymap.set('n', '<leader>Y', [["+Y]])
 
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
+
+--
+-- Harpoon-like bookmarking
+--
+vim.keymap.set("n", "<C-1>", "mA")
+vim.keymap.set("n", "<C-2>", "mB")
+vim.keymap.set("n", "<C-3>", "mC")
+vim.keymap.set("n", "<C-4>", "mD")
+
+vim.keymap.set("n", "<leader>1", "`A")
+vim.keymap.set("n", "<leader>2", "`B")
+vim.keymap.set("n", "<leader>3", "`C")
+vim.keymap.set("n", "<leader>4", "`D")
