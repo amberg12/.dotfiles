8550a87957dad739c2264aff97b9b7f8e19ac5e2
Amber Goulding <gouldinga1@cardiff.ac.uk>
2024-11-23 00:06:18 +0000

    add themes and timing

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,18 +1,21 @@
+;; Emacs options
 (setq inhibit-startup-message t
       auto-save-default nil
       make-backup-files nil
       custom-file "~/.config/emacs/custom.el")
-
-(load custom-file)
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (global-display-line-numbers-mode 1)
 (setq display-line-numbers-type 'relative)
-
 (setq scroll-conservatively 9999)
 
+(load custom-file)
+
+(set-frame-font "JetBrainsMono NF 18" nil t)
+
+;; Setup package-manager
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
@@ -24,8 +27,19 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; Setup evil
 (use-package evil
   :config
   (evil-mode 1))
 
-(set-frame-font "JetBrainsMono NF 18" nil t)
+;; Setup colorscheme
+(add-to-list 'custom-theme-load-path "~/.config/emacs/themes")
+(use-package almost-mono-themes
+  :config
+  (add-hook 'after-init-hook
+	    (lambda () (load-theme 'almost-mono-white t))))
+
+;; Load time
+(setq initial-scratch-message
+      (format ";; This is the scratch buffer. Emacs took %s to load."
+	      (emacs-init-time)))
