e631a8700aeadc972ca8e162a1be12c452ecfc2c
Amber Goulding <gouldinga1@cardiff.ac.uk>
2024-11-27 13:33:41 +0000

    add lsp

--- nvim/.config/nvim/lua/x/lazy.lua
+++ nvim/.config/nvim/lua/x/lazy.lua
@@ -1,12 +1,12 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
+  'git',
+  'clone',
+  '--filter=blob:none',
+  'https://github.com/folke/lazy.nvim.git',
+  '--branch=stable', -- latest stable release
+  lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
@@ -24,3 +24,78 @@ require('telescope').setup({})
 local telescope_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', telescope_builtin.find_files, {})
 vim.keymap.set('n', '<leader>fj', telescope_builtin.live_grep)
+
+-- lsp
+local cmp = require('cmp')
+local cmp_lsp = require("cmp_nvim_lsp")
+local capabilities = vim.tbl_deep_extend(
+  "force",
+  {},
+  vim.lsp.protocol.make_client_capabilities(),
+  cmp_lsp.default_capabilities())
+
+require("fidget").setup({})
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+    "rust_analyzer",
+    "clangd",
+  },
+  handlers = {
+    function(server_name) -- default handler (optional)
+
+      require("lspconfig")[server_name].setup {
+        capabilities = capabilities
+      }
+    end,
+
+    ["lua_ls"] = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim", "it", "describe", "before_each", "after_each" },
+            }
+          }
+        }
+      }
+    end,
+  }
+})
+
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-n>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-p>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, -- For luasnip users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+vim.diagnostic.config({
+  -- update_in_insert = true,
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
+})
