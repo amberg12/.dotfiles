3dd4639c033ee8792578dd1b1e65c971ad16fc40
Amber Goulding <gouldinga1@cardiff.ac.uk>
2024-12-14 20:35:13 +0000

    add projectile ivy & counsel integration

--- emacs/.config/emacs/custom-file.el
+++ emacs/.config/emacs/custom-file.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(projectile counsel councel which-key swiper ivy)))
+ '(package-selected-packages
+   '(counsel-projectile projectile counsel councel which-key swiper ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -83,6 +83,7 @@
 (use-package projectile
   :diminish projectile-mode
   :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
   :bind-keymap
   ("C-c p" . projectile-command-map)
   :init
@@ -91,3 +92,7 @@
   (when (file-directory-p "~/Cardiff") ; University files
     (setq projectile-project-search-path '("~/Cardiff")))
   (setq projectile-switch-project-action #'projectile-dired))
+
+;; counsel-projectile integration
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
