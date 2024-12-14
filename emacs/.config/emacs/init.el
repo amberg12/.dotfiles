dd9fbea8f2a833c389d773e1feb3bf77649b6e24
Amber Goulding <gouldinga1@cardiff.ac.uk>
2024-12-14 15:50:37 +0000

    initialise packages

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -6,3 +6,21 @@
 (menu-bar-mode -1)
 
 (set-frame-font "JetBrainsMono Nerd Font Mono 22" nil t)
+
+;;; Initialise package system
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Portability fix since not all platforms will have use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
